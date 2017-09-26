06dbaec38674256025d81e64abc3fc97d9beab62
Stephanie Fu <StephaniefuU@gmail.com>
2017-09-25 23:32:26 -0700

    first factorial

--- Coderbyte.js
+++ Coderbyte.js
@@ -3,4 +3,16 @@ const firstReverse = (str) => {
   return str.split('').reverse().join('')
 }
 
-firstReverse('Hello how are you')
+console.log(firstReverse('Hello how are you'))
+
+//First Factorial
+function FirstFactorial(num) { 
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result = result * i
+  }
+  return result;       
+  }
+
+console.log(FirstFactorial(4))
+     
