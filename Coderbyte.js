fa7d6cdc6bc41fea59db7cf797179ef4cf4d6713
Stephanie Fu <StephaniefuU@gmail.com>
2017-09-27 10:29:54 -0700

    check nums

--- Coderbyte.js
+++ Coderbyte.js
@@ -85,3 +85,13 @@ function SimpleSymbols(str) {
 }  
 
 console.log(SimpleSymbols('++d+==+c+=a+'))
+
+//Check Nums
+function CheckNums(num1,num2) {
+  if (num1 === num2) {
+    return '-1'
+  } 
+  return num2 > num1 ? 'true' : 'false'
+} 
+
+console.log(CheckNums(4,4))
